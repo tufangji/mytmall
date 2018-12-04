9a6212652174f1defaf54205679708e525fb17af
tufangji <415702620@qq.com>
2018-12-04 19:28:08 +0800

    完工

--- script/default.js
+++ script/default.js
@@ -1,15 +1,19 @@
 $(function () {
-
+    //重要变量
     let $sideLeft = $(".sidebar-left"),
         $sideLeftLi = $sideLeft.find("li"),
         $surpermarketA = $(".surpermarket-select-title a"),
-        $returntop = $(".sidebar-left-returntop");
+        $returnLeftTop = $(".sidebar-left-returntop"),
+        $returnRightTop = $(".sidebar-right").find(".returntop"),
+        $sidebarRightVip = $(".sidebar-right-vip");
     const baseValue = 700, //scrolltop 的基础值
-        stepVlue = 660; //每一个项目的值
+        stepVlue = 660, //每一个项目的值
+        marginValue = 50; //每一个项目的margin值
+
 
 
     $("input[type=text]").focus(); //文本框获取焦点
-    new TabAuto({ //自动翻页
+    new TabAuto({ //无缝翻页
         conEle: document.querySelectorAll(".main-category-carousel .pic-carousel li"),
         tabEle: document.querySelectorAll(".main-category-carousel .pic-tip li"),
         Wrap: document.querySelector("#main .main-category-carousel")
@@ -23,8 +27,8 @@ $(function () {
         } else {
             $sideLeft.removeClass("show");
         }
-        $.each($sideLeftLi, index => { //滚到一定高度亮一个
-            if ($(this).scrollTop() > baseValue + stepVlue * index) {
+        $sideLeftLi.each(index => { //滚到一定高度亮一个
+            if ($(this).scrollTop() >= baseValue + stepVlue * index + stepVlue / 2 + marginValue) {
                 $sideLeftLi
                     .eq(index)
                     .addClass("on")
@@ -33,7 +37,7 @@ $(function () {
         });
     })
 
-    $.each($surpermarketA, function () {
+    $surpermarketA.each(function () {  //天猫超市
         $(this).on("mouseenter", () => {
             $(this)
                 .addClass("on")
@@ -42,13 +46,31 @@ $(function () {
         })
     });
 
-    $returntop.on("click", function() {
-        $(this).scrollTop(0);
-    }.call(document))
+    $returnLeftTop.add($returnRightTop).on("click", function () { //返回头部  两个不同jq对象可以同时绑定一个函数
+        $("body,html").animate({
+            scrollTop: 0
+        }, 200)
+    })
+
+    $sideLeftLi.each(function (index) { //左边侧栏每个li的点击事件
+        $(this).on("click", () => {
+            $("html,body").animate({
+                scrollTop: baseValue + stepVlue * index + stepVlue / 2 + marginValue
+            }, 200)
+        })
+    })
 
 
 
 
-
+    $sidebarRightVip.each(function () {
+        $(this).hover(function () {
+            // over
+            $(this).find(".right-vip-hide").addClass("show");
+        }, function () {
+            // out
+            $(this).find(".right-vip-hide").removeClass("show");
+        });
+    });
 
 })
